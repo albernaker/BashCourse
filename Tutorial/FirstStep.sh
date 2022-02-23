4093249de3acc256e5a82f1cdeacce55b01c52f6
Albert Mora <compe.devel@gmail.com>
2022-02-23 09:37:16 +0100

    add

---
+++ Tutorial/FirstStep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#We need to declare numbers if we want to use variables
+a=0
+b=2
+
+#We create a variable that we will give it the result  
+result=$(( $a + $b + 0 ))
+
+
+#Now we print the result.
+echo "Result is $result" 
+
+
